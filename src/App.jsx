86d4e3ff7cc7a5477d68dbede0967e2ac8684ae4
soura07 <nitsouradeep2022@gmail.com>
2024-05-05 15:32:36 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
+
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration, getGenres } from "./store/homeSlice";
 
@@ -11,67 +12,62 @@ import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
 import PageNotFound from "./pages/404/PageNotFound";
-import { all } from "axios";
-
-
 
 function App() {
-  const dispatch = useDispatch();
-  const { url } = useSelector((state) => state.home);
+    const dispatch = useDispatch();
+    const { url } = useSelector((state) => state.home);
+    console.log(url);
 
-  useEffect(() => {
-    fetchApiConfig();
-    genresCall();
-  }, []);
+    useEffect(() => {
+        fetchApiConfig();
+        genresCall();
+    }, []);
 
-  const fetchApiConfig = () => {
-    fetchDataFromApi("/configuration").then((res) => {
-        console.log(res);
-        
-        const url = {
-          backdrop: res.images.secure_base_url + "original",
-          poster: res.images.secure_base_url + "original",
-          profile: res.images.secure_base_url + "original",
+    const fetchApiConfig = () => {
+        fetchDataFromApi("/configuration").then((res) => {
+            console.log(res);
 
-          
-        };
+            const url = {
+                backdrop: res.images.secure_base_url + "original",
+                poster: res.images.secure_base_url + "original",
+                profile: res.images.secure_base_url + "original",
+            };
 
-        dispatch(getApiConfiguration(url));
-      });
-  };
+            dispatch(getApiConfiguration(url));
+        });
+    };
 
+    const genresCall = async () => {
+        let promises = [];
+        let endPoints = ["tv", "movie"];
+        let allGenres = {};
 
-  const genresCall = async () =>{
-      let promises = []
-      let endPoints = ["tv", "movie"]
-      let allGenres = {}
+        endPoints.forEach((url) => {
+            promises.push(fetchDataFromApi(`/genre/${url}/list`));
+        });
 
-      endPoints.forEach((url) => {
-        promises.push(fetchDataFromApi(`/genre/${url}/list`))
-      })
+        const data = await Promise.all(promises);
+        console.log(data);
+        data.map(({ genres }) => {
+            return genres.map((item) => (allGenres[item.id] = item));
+        });
 
-      const data = await Promise.all(promises);
-      console.log(data);
-      data.map(({genres}) => {
-        return genres.map((item) => (allGenres[item.id] = item))
-      });
+        dispatch(getGenres(allGenres));
+    };
 
-      dispatch(getGenres(allGenres));
-};
-
-  return (
-    <BrowserRouter>
-              <Header />
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/:mediaType/:id" element={<Details />} />
-                    <Route path="/search/:query" element={<SearchResult />} />
-                    <Route path="/explore/:mediaType" element={<Explore />} />
-                    <Route path="*" element={<PageNotFound />} />
-                  </Routes>
-              <Footer />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
